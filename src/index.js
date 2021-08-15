f837b823fb5679d83e47572242e9c6ffffe1d6ad
peterchege <peterchege442@gmail.com>
2021-08-15 19:53:10 +0300

    using useReducer to return the new state

--- src/index.js
+++ src/index.js
@@ -1,38 +1,27 @@
-/* eslint-disable jsx-a11y/alt-text */
-import React, { useState, useEffect} from 'react';
+import React, { useReducer} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
 
-function GithubUser({ login }) {
-  const [data, setData] = useState(null);
 
-  useEffect(() => {
-    fetch(`https://api.github.com/users/${login}`)
-      .then(res => res.json())
-      .then(setData)
-      .catch(console.error);
-  }, [login]);
+function Checkbox() {
+  const [checked, toggle] = useReducer(checked => !checked ,false);
 
-  if (data){
-      return(
-        <div>
-          <h1>{data.login}</h1>
-          <img src={data.avatar_url} width={100} />
-          <p>{data.bio}</p>
-        </div>
-      );
-  }
-    return null;
-}
-
-function App() {
-  return <GithubUser login="peterchege"/>;
+    return(
+      <>
+          <input
+            type="checkbox"
+            value={checked}
+            onChange={toggle}
+            />
+            {checked ? "Checked" : "Not Checked" }                 
+      </>
+    );
 }
 
 
 ReactDOM.render(
-  <App />,
+  <Checkbox />,
   document.getElementById('root')
 );
 
