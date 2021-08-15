9f68666ae4b73ef689b0d970c79c75e4122db8c5
peterchege <peterchege442@gmail.com>
2021-08-15 19:32:43 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -5,34 +5,11 @@ import './index.css';
 
 
 function App() {
-  const [val, setVal] = useState("");
-  const [val2, setVal2] = useState("");
 
-  useEffect(() => {
-   console.log(`Field 1: ${val}`);
-  }, [val]);
-
-  useEffect(() => {
-    console.log(`Field 2: ${val2}`);
-   }, [val2]);
-
+  
     return(
       <>
-        <label>
-          Favourite Phase: 
-          <input
-            value={val}
-            onChange={e => setVal(e.target.value)}
-          />
-        </label>
-        <br />
-        <label>
-          Favourite Phase: 
-          <input
-            value={val2}
-            onChange={e => setVal2(e.target.value)}
-          />
-        </label>
+       
                     
       </>
     );
