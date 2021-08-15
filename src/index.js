549492c1ce21fed85f0c6484f56e6fc27bacb5db
peterchege <peterchege442@gmail.com>
2021-08-15 19:44:59 +0300

    using useEffect to fetch data from an API

--- src/index.js
+++ src/index.js
@@ -1,18 +1,33 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React, { useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
 
+function GithubUser({ login }) {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${login}`)
+      .then(res => res.json())
+      .then(setData)
+      .catch(console.error);
+  }, [login]);
+
+  if (data){
+      return(
+        <div>
+          <h1>{data.login}</h1>
+          <img src={data.avatar_url} width={100} />
+          <p>{data.bio}</p>
+        </div>
+      );
+  }
+    return null;
+}
 
 function App() {
-
-  
-    return(
-      <>
-       
-                    
-      </>
-    );
+  return <GithubUser login="peterchege"/>;
 }
 
 
